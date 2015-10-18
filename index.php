da6d47977ca7ab33ebda7188d8406acff205b4c6
Андреев Алексей <bezumny.tochk@gmail.com>
2015-10-18 20:38:01 +0300

    Добавлен контент на главную страницу

--- index.php
+++ index.php
@@ -9,21 +9,34 @@ function __autoload($class_name)
 
 $main = new page_init();
 $main->std_page_init();
-$content = "<br /><br />test";/*
-if ((isset($_SESSION['log_err'])) && ($_SESSION['log_err'] == 1)) {
-    $content = $content . "<br /><h2><center>Неправильный логин или пароль</center></h2><br />";
-    $_SESSION['log_err'] = 0;
+$content = "";
+$query = "SELECT * FROM `posts` ORDER BY `id` DESC";
+$result = mysql_query($query) or die(mysql_error());
+$posts = mysql_fetch_array($result, MYSQL_ASSOC);
+$a = mysql_num_rows($result);
+if ($a > 2) $a = 2;
+for ($i = 0; $i < $a; $i++) {
+    $content .= "<div id='block_news'><div id='block_in_block_news'><div id='block_in_block_news_2'><div id='block_news_head' style='font-size:20px;'  >{$posts['theme']}</div>
+<div id='block_news_content' style='font-size:16px;'>{$posts['short_text']}</div></div></div><div id='block_news_info'><div id='block_news_info_2'><div id='block_date' style='font-size: 20px;'>{$posts['time']}</div>
+<div id='block_data'><div id='data_author'> Author<br><text_green>{$posts['author']}</text_green></div><div id='data_category'> Category<br><text_green>{$posts['category']}</text_green></div>
+<div id='data_com'> Comments<br><text_green>{$posts['comments']}</text_green></div></div><div id='block_info_footer' style='font-size: 20px;'> read more</div></div></div>";
+//<div id='block_save' style='font-size: 20px;'> Save + </div></div>";
+    //if ($admin == 1) $content = $content . " <a href=/news/edit.php?id={$news['id']}>Редактировать</a>";
+    //$content = $content . "</h3><br />{$news['content']}<br /></div><br />";
+    $posts = mysql_fetch_array($result, MYSQL_ASSOC);
 }
-$query = "SELECT * FROM `{$config['table_prefix']}index` ORDER BY pr DESC";
+$query = "SELECT * FROM `projects` ORDER BY `id` DESC";
 $result = mysql_query($query) or die(mysql_error());
 $index = mysql_fetch_array($result, MYSQL_ASSOC);
 while ($index) {
-    if ($main->admin == 1) $content = $content . "<a href='/admin/index_edit.php?id={$index['id']}'>Редактировать</a><a href='/admin/index_process.php?do=delete&id={$index['id']}'>Удалить</a>";
-    if ($index['img_url'] == '') $index['img_url'] = "/design/images/index/logo.png";
-    $content = $content . "<br /><div id='block_content'><a href='{$index['url']}' target='_blank'><img src='{$index['img_url']}' align='left' vspace='5' hspace='5'><h3>{$index['name']}</h3></a>{$index['comment']}</div>";
+    if ($index['img_url'] == '') $index['img_url'] = "/design/images/logo.png";
+    $content .= "<div id='block_new_sait'> <div id='block_in_block_sait'><div id='block_in_block_sait_2'><div id='block_sait_head'>{$index['name']}</div><div id='block_sait_scrin_info'>
+<div class='block_sait_scrin_1'><div class='block_sait_scrin_2'   style='background-image: url(\"{$index['img_url']}\")'></div>
+<div class='block_sait_scrin_3'>{$index['comment']}</div></div></div></div></div><div id='block_sait_info'><div id='block_sait_info_2'>
+<div id='sait_date' style='width: 100px'>{$index['time']}</div><div id='sait_data_about_block' style='width: 180px'><div style='width: 80px' id='sait_author'>Author<br><text_green>{$index['author']}</text_green></div>
+<div style='width: 80px' id='sait_cat'>Category<br><text_green>{$index['category']}</text_green></div></div><div id='sait_right_footer'>read more</div>
+</div></div></div>";
     $index = mysql_fetch_array($result, MYSQL_ASSOC);
 }
-if ($main->admin == 1) $content = $content . "<h2><a href='/admin/index_create.php' style='margin: 0;'>Добавить ссылку на проект</a></h2>";
-else $content = $content . "<br />";*/
 $main->timer_save();
 $main->pjax_init($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
