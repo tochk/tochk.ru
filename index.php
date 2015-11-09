32aefd7491192817e21b22b66b1057be3bf73a11
Андреев Алексей <bezumny.tochk@gmail.com>
2015-11-09 13:51:47 +0300

    Изменён порядок отображения контента на главной странице

--- index.php
+++ index.php
@@ -10,6 +10,21 @@ function __autoload($class_name)
 $main = new page_init();
 $main->std_page_init();
 $content = "";
+$query = "SELECT * FROM `projects` ORDER BY `id` DESC";
+$result = mysql_query($query) or die(mysql_error());
+$index = mysql_fetch_array($result, MYSQL_ASSOC);
+$a = mysql_num_rows($result);
+if ($a > 2) $a = 2;
+for ($i = 0; $i < $a; $i++) {
+    if ($index['img_url'] == '') $index['img_url'] = "/design/images/logo.png";
+    $content .= "<div id='block_new_sait' onclick=\"window.open('{$index['url']}', '_blank');\"> <div id='block_in_block_sait'><div id='block_in_block_sait_2'><div id='block_sait_head'>{$index['name']}</div><div id='block_sait_scrin_info'>
+<div class='block_sait_scrin_1'><div class='block_sait_scrin_2'   style='background-image: url(\"{$index['img_url']}\")'></div>
+<div class='block_sait_scrin_3'>{$index['comment']}</div></div></div></div></div><div id='block_sait_info'><div id='block_sait_info_2'>
+<div id='sait_date' style='width: 100px'>{$index['time']}</div><div id='sait_data_about_block' style='width: 180px'><div style='width: 80px' id='sait_author'>Author<br><text_green>{$index['author']}</text_green></div>
+<div style='width: 80px' id='sait_cat'>Category<br><text_green>{$index['category']}</text_green></div></div><div id='sait_right_footer'>read more</div>
+</div></div></div>";
+    $index = mysql_fetch_array($result, MYSQL_ASSOC);
+}
 $query = "SELECT * FROM `posts` ORDER BY `id` DESC";
 $result = mysql_query($query) or die(mysql_error());
 $posts = mysql_fetch_array($result, MYSQL_ASSOC);
@@ -32,19 +47,6 @@ for ($i = 0; $i < $a; $i++) {
 <div id='block_save' style='font-size: 20px;'> Save + </div></div>";
     //if ($admin == 1) $content = $content . " <a href=/news/edit.php?id={$news['id']}>Редактировать</a>";
     $posts = mysql_fetch_array($result, MYSQL_ASSOC);
-}
-$query = "SELECT * FROM `projects` ORDER BY `id` DESC";
-$result = mysql_query($query) or die(mysql_error());
-$index = mysql_fetch_array($result, MYSQL_ASSOC);
-while ($index) {
-    if ($index['img_url'] == '') $index['img_url'] = "/design/images/logo.png";
-    $content .= "<div id='block_new_sait' onclick=\"window.open('{$index['url']}', '_blank');\"> <div id='block_in_block_sait'><div id='block_in_block_sait_2'><div id='block_sait_head'>{$index['name']}</div><div id='block_sait_scrin_info'>
-<div class='block_sait_scrin_1'><div class='block_sait_scrin_2'   style='background-image: url(\"{$index['img_url']}\")'></div>
-<div class='block_sait_scrin_3'>{$index['comment']}</div></div></div></div></div><div id='block_sait_info'><div id='block_sait_info_2'>
-<div id='sait_date' style='width: 100px'>{$index['time']}</div><div id='sait_data_about_block' style='width: 180px'><div style='width: 80px' id='sait_author'>Author<br><text_green>{$index['author']}</text_green></div>
-<div style='width: 80px' id='sait_cat'>Category<br><text_green>{$index['category']}</text_green></div></div><div id='sait_right_footer'>read more</div>
-</div></div></div>";
-    $index = mysql_fetch_array($result, MYSQL_ASSOC);
 }
 $main->timer_save();
 $main->pjax_init($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
