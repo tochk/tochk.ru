b4d128ae79aa8f37148bf2b7ccfd16ce47ccc5ed
Alexey Andreev <bezumny.tochk@gmail.com>
2016-03-06 22:49:13 +0300

    /portfolio/index.php mysqli

--- engine/classes/Data.php
+++ engine/classes/Data.php
@@ -23,6 +23,27 @@ class Data
         "</div></div></div>";
     }
 
+    public function printPortfolio($row)
+    {
+        if ($row['img_url'] == '') $row['img_url'] = "/design/images/logo.png";
+        return "<div id='block_new_sait' onclick=\"window.open('{$row['url']}', '_blank');\">" .
+        "<div id='block_in_block_sait'>" .
+        "<div id='block_in_block_sait_2'>" .
+        "<div id='block_sait_head'>{$row['name']}</div>" .
+        "<div id='block_sait_scrin_info'>" .
+        "<div class='block_sait_scrin_1'>" .
+        "<div class='block_sait_scrin_2' style='background-image: url(\"{$row['img_url']}\")'></div>" .
+        "<div class='block_sait_scrin_3'>{$row['comment']}</div>" .
+        "</div></div></div></div><div id='block_sait_info'>" .
+        "<div id='block_sait_info_2'>" .
+        "<div id='sait_date' style='width: 100px'>{$row['time']}</div>" .
+        "<div id='sait_data_about_block' style='width: 180px'>" .
+        "<div style='width: 80px' id='sait_author'>Author<br><text_green>{$row['author']}</text_green></div>" .
+        "<div style='width: 80px' id='sait_cat'>Category<br><text_green>{$row['category']}</text_green></div>" .
+        "</div><div id='sait_right_footer'>read more</div>" .
+        "</div></div></div>";
+    }
+
     public function printPost($connection, $row)
     {
         $postTags = $this->getPostTags($connection, $row['id']);
