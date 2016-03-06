548c3b39ec4258d4d8c74861e8da83d110844b65
Alexey Andreev <bezumny.tochk@gmail.com>
2016-03-06 22:01:33 +0300

    Исправлена авторизация

--- engine/classes/User.php
+++ engine/classes/User.php
@@ -2,5 +2,49 @@
 
 class User
 {
+    public $id;
+    public $login;
+    public $password;
+    public $salt;
+    public $email;
 
+    public function __construct()
+    {
+
+    }
+
+    public function getSaltFromDb($mysql, $login)
+    {
+        $query = "SELECT `salt` FROM `users` WHERE `login`=?";
+        $stmt = $mysql->connection->prepare($query);
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows != 1) {
+            return false;
+        }
+        $salt = '';
+        $stmt->bind_result($salt);
+        $stmt->fetch();
+        $stmt->close();
+        return $salt;
+    }
+
+    public function checkPassword($mysql, $login, $password, $salt)
+    {
+        $hashed_password = hash('sha256', hash('sha256', $password) . $salt);
+        $query = "SELECT `id` FROM `users` WHERE `login`=? AND `password`=?";
+        $stmt = $mysql->connection->prepare($query);
+        $stmt->bind_param("ss", $login, $hashed_password);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows != 1) {
+            return false;
+        }
+        $id = '';
+        $stmt->bind_result($id);
+        $stmt->fetch();
+        $stmt->close();
+        return $id;
+    }
 }
--- login.php
+++ login.php
@@ -18,41 +18,25 @@ if ($_GET['logout'] == 1) {
     unset($_SESSION['id']);
     unset($_SESSION['login']);
     header("Location: /");
-    exit();
+    exit;
 }
 
 $login = isset($_POST["login"]) ? $_POST["login"] : null;
 $password = isset($_POST["password"]) ? $_POST["password"] : null;
 
 if ($login && $password) {
-    $query = "SELECT `salt` FROM `users` WHERE `login`=?";
-    $stmt = $mysql->connection->prepare($query);
-    $stmt->bind_param("s", $login);
-    $stmt->execute();
-    $stmt->store_result();
-    if ($stmt->num_rows != 1) {
-        echo "cant't find user or find more than one user";
+    if ($salt = $user->getSaltFromDb($mysql, $login)) {
+        if ($id = $user->checkPassword($mysql, $login, $password, $salt)) {
+            $_SESSION['id'] = $id;
+            $_SESSION['login'] = $login;
+        } else {
+            echo "wrong password";
+            exit;
+        }
+    } else {
+        echo "wrong login";
         exit;
     }
-    $salt = '';
-    $stmt->bind_result($salt);
-    $stmt->fetch();
-    $stmt->close();
-    $hashed_password = hash('sha256', hash('sha256', $password) . $salt);
-    $query = "SELECT `id` FROM `users` WHERE `login`=? AND `password`=?";
-    $stmt = $mysql->connection->prepare($query);
-    $stmt->bind_param("ss", $login, $hashed_password);
-    $stmt->execute();
-    $stmt->store_result();
-    if ($stmt->num_rows != 1) {
-        echo "wrong password";
-        exit;
-    }
-    $id = '';
-    $stmt->bind_result($id);
-    $stmt->fetch();
-    $_SESSION['id'] = $id;
-    $_SESSION['login'] = $login;
 } else {
     echo "login or/and password are empty";
     exit;
