5f57a48efe863b49c8af3f1b2add0e512a7f66ec
Alexey Andreev <bezumny.tochk@gmail.com>
2016-03-06 22:45:25 +0300

    /admin/* mysqli

--- admin/logs.php
+++ admin/logs.php
@@ -1,36 +1,29 @@
 <?php
 session_start();
 $title = "Логи пользователей";
-require($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/engine/helpers.php');
 function __autoload($class_name)
 {
     require_once $_SERVER['DOCUMENT_ROOT'] . '/engine/classes/' . $class_name . '.php';
 }
 
-$main = new page_init();
-$main->std_page_init();
-if ($main->admin == 0) {
+$page = new Page();
+$mysql = new Mysql();
+$mysql->connect($page->getMysqlHost(), $page->getMysqlLogin(), $page->getMysqlPassword(), $page->getMysqlDb(), $page->debugLevel);
+$user = new User($mysql);
+if ($user->isAdmin != 1) {
     header('Location: /');
-    exit();
+    exit;
 }
+$logs = new Logs();
+$data = new Data();
 $content = "<br /><div style='text-align: center;'><h1>Логи пользователей</h1><br />
-<h3><a href='/admin/'>Панель управления сайтом</a> ||| 
-<a href='/admin/stat.php'>Статистика посещений</a> ||| 
-<a href='/admin/users.php'>Список пользователей</a> ||| 
+<h3><a href='/admin/'>Панель управления сайтом</a> |||
+<a href='/admin/stat.php'>Статистика посещений</a> |||
+<a href='/admin/users.php'>Список пользователей</a> |||
 Логи<br /></h3>
 </div>
 <form action='/admin/logs.php?do=show' method='post'><br />
 Введите ID пользователя : <input type='text' name='id' value='{$_POST['id']}' />
 <input type='submit' value=' Показать ' /></form>";
-if ($_GET['do'] == "show") {
-    $user_id = mysql_real_escape_string($_POST['id']);
-    mysql_select_db("logs") or die (mysql_error());
-    $result = mysql_query("SELECT * FROM `logs_$user_id` ORDER BY id DESC");
-    $myrow = mysql_fetch_array($result, MYSQL_ASSOC);
-    for ($i = 0; $i < mysql_num_rows($result); $i++) {
-        $content .= "<div style='text-align: center;'><h4>ID = {$myrow['id']} ACTION = {$myrow['action']} RESULT = {$myrow['result']} TIME=" . date('H:i d.m.Y', $myrow['time']) . "</h4></div>";
-        $myrow = mysql_fetch_array($result, MYSQL_ASSOC);
-    }
-}
-$main->timer_save();
-$main->pjax_init($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
+$page->printPage($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
--- admin/stat.php
+++ admin/stat.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
 $title = "Статистика посещений";
-require($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/engine/helpers.php');
 function __autoload($class_name)
 {
     require_once $_SERVER['DOCUMENT_ROOT'] . '/engine/classes/' . $class_name . '.php';
 }
 
-$main = new page_init();
-$main->std_page_init();
-if ($main->admin == 0) {
+$page = new Page();
+$mysql = new Mysql();
+$mysql->connect($page->getMysqlHost(), $page->getMysqlLogin(), $page->getMysqlPassword(), $page->getMysqlDb(), $page->debugLevel);
+$user = new User($mysql);
+if ($user->isAdmin != 1) {
     header('Location: /');
-    exit();
+    exit;
 }
+$logs = new Logs();
+$data = new Data();
 $content = "<br /><div style='text-align: center;'><h1>Статистика посещений</h1>
 <br/>
 <h3><a href='/admin/'>Панель управления сайтом</a> |||
 Статистика посещений |||
 <a href='/admin/users.php'>Список пользователей</a> |||
 <a href='/admin/logs.php'>Логи</a></h3><br /></div>";
-$main->timer_save();
-$main->pjax_init($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
+$page->printPage($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
