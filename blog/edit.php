881572b6ac993cd0968f54e6f242a35158016196
Alexey Andreev <bezumny.tochk@gmail.com>
2016-04-15 19:50:56 +0300

    Добавлены страницы создания, редактирования и просмотра постов

---
+++ blog/edit.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$logs = new Logs();
+$title = "Редактировать запись";
+require($_SERVER['DOCUMENT_ROOT'] . '/engine/helpers.php');
+function __autoload($class_name)
+{
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/engine/classes/' . $class_name . '.php';
+}
+
+$page = new Page();
+$mysql = new Mysql();
+$mysql->connect($page->getMysqlHost(), $page->getMysqlLogin(), $page->getMysqlPassword(), $page->getMysqlDb(), $page->debugLevel);
+$user = new User($mysql);
+$data = new Data();
+$logs->setCreateClasses();
+$content = "";
+
+$page->printPage($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
+$logs->setEnd();
+$logs->writeToDb($mysql);
---
+++ blog/new.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$logs = new Logs();
+$title = "Создать запись";
+require($_SERVER['DOCUMENT_ROOT'] . '/engine/helpers.php');
+function __autoload($class_name)
+{
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/engine/classes/' . $class_name . '.php';
+}
+
+$page = new Page();
+$mysql = new Mysql();
+$mysql->connect($page->getMysqlHost(), $page->getMysqlLogin(), $page->getMysqlPassword(), $page->getMysqlDb(), $page->debugLevel);
+$user = new User($mysql);
+$data = new Data();
+$logs->setCreateClasses();
+$content = "";
+
+$page->printPage($content, $_SERVER['DOCUMENT_ROOT'] . '/design/html/main.php', $title);
+$logs->setEnd();
+$logs->writeToDb($mysql);
